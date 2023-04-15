caa4637a0f12454a0854a7563bd36dfa00f38f65
zaharlol <kovtun.zahar12@mail.ru>
2023-04-15 17:18:30 +0300

    Добавьте файлы проекта.

---
+++ Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Exception exception = new Exception("Собственное исключение");
+        exception.Data.Add("Время исключения: ", DateTime.Now);
+        exception.HelpLink = "www.Google.com";
+        try
+        {
+            Console.WriteLine("ЛоЛКеК");
+            throw new ArgumentOutOfRangeException("Сообщение об ошибке");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Конец программы");
+        }
+        Console.ReadKey();
+
+        try
+        {
+            Console.WriteLine("ЛоЛКеК");
+            throw new RankException("Сообщение об ошибке");
+        }
+        catch (RankException ex)
+        {
+            Console.WriteLine(ex.GetType);
+        }
+        finally
+        {
+            Console.WriteLine("Конец программы");
+        }
+        Console.ReadKey();
+    }
+}
