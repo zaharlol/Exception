6f7b28958b11095c0084a2c54a60536814c018f9
zaharlol <kovtun.zahar12@mail.ru>
2023-04-17 20:01:55 +0300

    Шаблоны

--- Делегаты/Program.cs
+++ Делегаты/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    public delegate void Minus(int a, int b);
+    static void Main(string[] args)
+    {
+        Action showMessageDelegate = ShowMessage;
+        showMessageDelegate.Invoke();
+
+        Func<int, int, int, int> sumDelegate = Sum;
+        int result = sumDelegate.Invoke(1, 30, 120);
+        Console.WriteLine(result);
+
+        Predicate <string> checkLengthDelegate = CheckLength;
+        bool status = checkLengthDelegate.Invoke("skill_factory");
+        Console.WriteLine(status);
+    }
+    static void Minusus(int a, int b)
+    {
+        int c = b - a;
+        Console.WriteLine(c);
+    }
+    static void Plus(int a, int b)
+    {
+        int c = b + a;
+        Console.WriteLine(c);
+    }
+    static void ShowMessage()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    static int Sum(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+    static bool CheckLength(string _row)
+    {
+        if (_row.Length > 3) return true;
+        return false;
+    }
+}
