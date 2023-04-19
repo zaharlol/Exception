9f51a41b6c8d6bee32e5f6358efa14fcf36f434e
zaharlol <kovtun.zahar12@mail.ru>
2023-04-19 12:32:04 +0300

    Задание

--- Ковариантность/Program.cs
+++ Ковариантность/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public delegate Car car();
+    public delegate Parent par();
+    delegate void  ChildI (Child child);
+    public static Car cars()
+    {
+        return null;
+    }
+    public static Lexus LXs()
+    {
+        return null;
+    }
+    public static Parent pars()
+    {
+        return null;
+    }
+    public static Child Chs()
+    {
+        return null;
+    }
+    static void Main(string[] args)
+    {
+        car car = cars;
+        car car1 = LXs;
+
+        ChildI child = GPI;
+        child.Invoke(new Child());
+        
+        Console.Read();
+    }
+    public static void GPI(Parent p)
+    {
+        Console.WriteLine(p.GetType());
+    }
+}
+class Car { }
+class Lexus : Car { }
+class Parent { }
+class Child : Parent { }
