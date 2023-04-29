e77d03888eff333f4b2cc5ed1e34a493299682d4
zaharlol <kovtun.zahar12@mail.ru>
2023-04-29 14:25:30 +0300

    Готово

--- SR/Program.cs
+++ SR/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Exc exce = new Exc();
+        string[] exc = { "IndexOutOfRangeException", "InvalidOperationException ","KeyNotFoundException", "otImplementedException"};
+        var exc1 = exc[0]; 
+
+        try
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                new Exception{};
+                Person person = new Person { Name = "Tom", Age = 17 };
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
+
+class Person
+{
+    private int age;
+    public string Name { get; set; } = "";
+    public int Age
+    {
+        get => age;
+        set
+        {
+            if (value < 18)
+                throw new Exception("Лицам до 18 регистрация запрещена");
+            else
+                age = value;
+        }
+    }
+}
+
+enum Exc
+{
+    IndexOutOfRangeException, 
+    InvalidOperationException,  
+    KeyNotFoundException,
+    otImplementedException
+}
---
+++ SR2/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        NumberRad numberRad = new NumberRad();
+        numberRad.NumberReadEvent += ShowNumber;
+
+        while (true) 
+        {
+            try
+            {
+                numberRad.Read();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение");
+            }
+        }
+        
+        static void ShowNumber(int number)
+        {
+            switch (number)
+            {
+                case 1: Plus(); break;
+                case 2: Minus(); break;
+            }
+        }
+        static void Plus()
+        {
+            string[] words = { "Аня", "Лера", "Петя", "Катя", "Вася" };
+
+            IEnumerable<string> query = from word in words
+                                        orderby word.Substring(0, 1)
+                                        select word;
+
+            foreach (string str in query)
+                Console.WriteLine(str);
+        }
+        static void Minus()
+        {
+            string[] words = { "Аня", "Лера", "Петя", "Катя", "Вася" };
+
+            IEnumerable<string> query = from word in words
+                                        orderby word.Substring(0, 1) descending
+                                        select word;
+
+            foreach (string str in query)
+                Console.WriteLine(str);
+        }
+    }
+}
+
+class NumberRad
+{
+    public delegate void NumberRadDelegate(int number);
+    public event NumberRadDelegate NumberReadEvent;
+
+    public void Read()
+    {
+        Console.WriteLine("Число 1 (А-Я), число 2 (Я-А)");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number != 1 && number != 2) throw new FormatException();
+
+        NumberEntered(number);
+    }
+     protected virtual void NumberEntered(int number)
+    {
+        NumberReadEvent?.Invoke(number);
+    }
+}
